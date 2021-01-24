d446f6c723cc223cd41409324c7ad36774679a01
Anmora96 <anmora.2396@gmail.com>
2021-01-24 00:12:08 +0000

    Ejercicio 18

--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -2,6 +2,15 @@
 //This is a comment
 // Declare and write your function here:
 
+function standardsMaker(){
+	
+	for(var i = 0; i <= 300; i++)
+	{
+		console.log("Yo preguntaré si estoy atascado");
+	}
+	
+	return i;
+}
 
 // Calling the function:
-standardsMaker();
+standardsMaker(i);
